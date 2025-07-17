0085c708258a3119c58ad12d8026ca4db6d65ba9
ameleiral <meilawther4@gmail.com>
2025-07-17 22:35:43 +0300

    модальные окна открываются

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -34,31 +34,33 @@ export default {
 </script>
 
 <style scoped>
+
 .header {
   width: 100%;
   margin: 0 auto;
   background-color: #FFFFFF;
-  position: relative;
 }
-
 .header__block {
   height: 70px;
   display: flex;
+  flex-wrap: nowrap;
   align-items: center;
   justify-content: space-between;
+  position: relative;
+  top: 0;
+  left: 0;
   padding: 0 10px;
 }
-
 .header__logo img {
   width: 85px;
 }
-
 .header__nav {
+  max-width: 290px;
+  padding: 0;
   display: flex;
   align-items: center;
-  gap: 20px;
+  justify-content: center;
 }
-
 .header__btn-main-new {
   width: 178px;
   height: 30px;
@@ -67,37 +69,49 @@ export default {
   color: #FFFFFF;
   border: none;
   font-size: 14px;
-  cursor: pointer;
+  line-height: 1;
+  font-weight: 500;
+  margin-right: 20px;
 }
-
+.header__btn-main-new a {
+  color: #FFFFFF;
+}
 .header__user {
+  height: 20px;
   display: flex;
+  flex-wrap: nowrap;
   align-items: center;
-  gap: 5px;
-  cursor: pointer;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 20px;
   color: #565EEF;
 }
-
-.user-arrow {
+.header__user::after {
+  content: "";
   display: block;
   width: 6px;
   height: 6px;
+  border-radius: 1px;
   border-left: 1.9px solid #565EEF;
   border-bottom: 1.9px solid #565EEF;
   transform: rotate(-45deg);
+  margin: -6px 0 0 5px;
+  padding: 0;
 }
-
 .header__pop-user-set {
+  display: none;
   position: absolute;
-  top: 60px;
+  top: 61px;
   right: 0;
   width: 213px;
-  padding: 34px;
-  background: #FFF;
+  height: 205px;
   border-radius: 10px;
   border: 0.7px solid rgba(148, 166, 190, 0.4);
+  background: #FFF;
   box-shadow: 0px 10px 39px 0px rgba(26, 56, 101, 0.21);
-  z-index: 100;
+  padding: 34px;
+  text-align: center;
+  z-index: 2;
 }
 
 .pop-user-set__name {
@@ -108,7 +122,6 @@ export default {
   letter-spacing: -0.14px;
   margin-bottom: 4px;
 }
-
 .pop-user-set__mail {
   color: #94A6BE;
   font-size: 14px;
@@ -116,21 +129,18 @@ export default {
   letter-spacing: -0.14px;
   margin-bottom: 10px;
 }
-
 .pop-user-set__theme {
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin-bottom: 30px;
 }
-
 .pop-user-set__theme p {
   color: #000;
   font-size: 14px;
   line-height: 21px;
   letter-spacing: -0.14px;
 }
-
 .pop-user-set__theme input[type=checkbox] {
   position: relative;
   width: 24px;
@@ -139,10 +149,9 @@ export default {
   background: #EAEEF6;
   outline: none;
   -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
+     -moz-appearance: none;
+          appearance: none;
 }
-
 .pop-user-set__theme input[type=checkbox]::before {
   content: "";
   position: absolute;
@@ -154,11 +163,9 @@ export default {
   background-color: #94A6BE;
   transition: 0.5s;
 }
-
 .pop-user-set__theme input:checked[type=checkbox]::before {
   left: 12px;
 }
-
 .pop-user-set button {
   width: 72px;
   height: 30px;
@@ -166,21 +173,9 @@ export default {
   color: #565EEF;
   border-radius: 4px;
   border: 1px solid #565EEF;
-  transition: all 0.3s ease;
 }
-
-.pop-user-set button:hover {
-  background-color: #565EEF;
-  color: #FFFFFF;
-}
-
-.pop-user-set button:hover a {
-  color: #FFFFFF;
-}
-
 .pop-user-set button a {
   color: #565EEF;
-  transition: all 0.3s ease;
 }
 
 @media screen and (max-width: 495px) {
